30e57e6927ba767a710c18a8907aa9da7c1521d1
hbelitz <hbelitz@b6546003-d6fc-0310-bf9d-abe751a17290>
2006-05-17 12:56:26 +0000

    Corrected computation. Done some speed-ups.

---
+++ trunk/evaltool/src/cdiscrepancymeasures.cpp
@@ -0,0 +1,211 @@
+//
+// C++ Implementation: cdiscrepancymeasures
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "cdiscrepancymeasures.h"
+
+using namespace std;
+using namespace boost;
+
+CDiscrepancyMeasures::CDiscrepancyMeasures( ulong ulID )
+ throw()
+:	CFilter( ulID, "Discrepancy measures", 2, 0, "CDiscrepancyMeasures", "0.1", "CFilter" )
+{
+  setModuleID( "Bla" );
+  
+  sDocumentation = "Creates a gradient field from the input image via a gaussian derivative\n"
+                   "** Input ports:\n"
+                   "0: A scalar single channel data set (image data, required)\n"
+									 "1: A scalar single channel data set (region of interest, optional)\n"
+                   "** Output ports:\n"
+                   "0: A 2D or 3D vector field which is normalized\n"
+									 "** Parameters:\n"
+									 "Sigma: Width of gaussian";
+
+	parameters.initDouble( "DiceCoefficient", 0.5, 0.0, 100000.0 );
+	parameters.initDouble( "TanimotoCoefficient", 0.5, 0.0, 100000.0 );
+	parameters.initDouble( "HausdorffDistance", 0.5, 0.0, 100000.0 );
+	parameters.initDouble( "MeanDistance", 0.5, 0.0, 100000.0 );
+	parameters.initUnsignedLong( "InputRegionSize", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "ReferenceRegionSize", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "SharedRegionSize", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "InputSurface", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "ReferenceSurface", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "CombinedArea", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "FalsePositives", 0, 0, 10000000 );
+	parameters.initUnsignedLong( "FalseNegatives", 0, 0, 10000000 );
+  inputsVec[0].portType = CPipelineItem::IOInteger;
+	inputsVec[1].portType = CPipelineItem::IOInteger;
+  //outputsVec[0].portType = CPipelineItem::IOVector;
+}
+
+
+CDiscrepancyMeasures::~CDiscrepancyMeasures() throw()
+{
+}
+
+
+void CDiscrepancyMeasures::apply() throw()
+{
+FBEGIN;
+  bModuleReady = false;
+  if ( getInput() )
+    call<Length<imageTL>::value - 1>();
+  else
+  {
+    alog << LWARN << SERROR("Input type is no 2D or 3D image!") << endl;
+    return;
+  }
+FEND; 
+}
+
+template<typename ImageType> bool CDiscrepancyMeasures::compute() throw()
+{
+  shared_ptr<ImageType> inputSPtr = static_pointer_cast<ImageType>( getInput() );
+	shared_ptr<ImageType> referenceSPtr = static_pointer_cast<ImageType>( getInput(1) );
+  if ( !checkInput<ImageType>( inputSPtr, 2, 3 ) || !checkInput<ImageType>( referenceSPtr, 2, 3 ) )
+    return false;
+  if ( inputSPtr->getExtent(0) != referenceSPtr->getExtent(0)
+  	|| inputSPtr->getExtent(1) != referenceSPtr->getExtent(1) )
+  	return false;
+  if( inputSPtr->getDimension() > 2 && inputSPtr->getExtent(2) != referenceSPtr->getExtent(2) )
+  	return false;
+  bModuleReady = true;
+	// Coefficients
+  double dDiceCoefficient = 0.0;
+  double dTanimotoCoefficient = 0.0;
+  // Compute region size and dice coefficient. Also mark boundary voxels for later
+  // distance computation
+  std::list<TPoint3D> anInputList;
+  std::list<TPoint3D> aReferenceList;
+  ulong ulInputRegionSize = 0;
+  ulong ulReferenceRegionSize = 0;
+  ulong ulSharedRegionSize = 0;
+  ulong ulInputSurface = 0;
+  ulong ulReferenceSurface = 0;
+  ulong ulCombinedArea = 0;
+  ulong ulFalsePositives = 0;
+  ulong ulFalseNegatives = 0;
+  cerr << "Computing area sizes... ";
+  typename ImageType::iterator inputIt = inputSPtr->begin();
+  typename ImageType::iterator referenceIt = referenceSPtr->begin();
+  while( inputIt != inputSPtr->end() )
+  {
+  	if ( (*inputIt) > 0 )
+  	{
+  		ulInputRegionSize++;
+  		ushort x,y,z;
+  		inputIt.getPos( x,y,z );
+  		if ( !( (*inputSPtr)(x+1,y,z) > 0 && (*inputSPtr)(x-1,y,z) > 0
+  			&& (*inputSPtr)(x,y+1,z) > 0 && (*inputSPtr)(x,y-1,z) > 0
+  			&& (*inputSPtr)(x,y,z+1) > 0 && (*inputSPtr)(x,y,z-1) > 0 ) )
+  		{
+  			anInputList.push_back( TPoint3D(x,y,z) );
+  			ulInputSurface++;
+  		}
+  		ulCombinedArea++;
+  	}
+  	if ( (*referenceIt) > 0 )
+  	{
+  		ulReferenceRegionSize++;
+  		ushort x,y,z;
+  		referenceIt.getPos( x,y,z );
+  		if ( !( (*referenceSPtr)(x+1,y,z) > 0 && (*referenceSPtr)(x-1,y,z) > 0
+  			&& (*referenceSPtr)(x,y+1,z) > 0 && (*referenceSPtr)(x,y-1,z) > 0
+  			&& (*referenceSPtr)(x,y,z+1) > 0 && (*referenceSPtr)(x,y,z-1) > 0 ) )
+  		{
+  			aReferenceList.push_back( TPoint3D(x,y,z) );
+  			ulReferenceSurface++;
+  		}
+  		if ( !( (*inputIt) > 0 ) )
+  			ulCombinedArea++;
+  	}
+  	if ( (*inputIt) > 0 && (*referenceIt) > 0 ) ulSharedRegionSize++;
+  	if ( (*inputIt) > 0 && !(*referenceIt) > 0 ) ulFalsePositives++;
+  	if ( !(*inputIt) > 0 && (*referenceIt) > 0 ) ulFalseNegatives++;
+  	++inputIt;
+  	++referenceIt;
+  }
+  cerr << "done.\nFound " << anInputList.size() << " segmentation and " << aReferenceList.size()
+   << " reference voxels" << endl;
+  cerr << "Computing coefficients... ";
+  // Compute Dice and Tanimoto coefficient
+	dDiceCoefficient = static_cast<double>( 2 * ulSharedRegionSize )
+		/ static_cast<double>( ulInputRegionSize + ulReferenceRegionSize );
+	dTanimotoCoefficient = static_cast<double>( ulSharedRegionSize )
+		/ static_cast<double>( ulCombinedArea );
+  // Compute distance measures between the two surfaces
+  std::list<TPoint3D>::iterator it = anInputList.begin();
+  std::list<TPoint3D>::iterator jt = aReferenceList.begin();
+  double dMinDist = inputSPtr->getExtent(0)*inputSPtr->getExtent(1)*inputSPtr->getExtent(2);
+  double dSumDist = 0.0;
+  double dMaxMin = 0.0;
+  while( it != anInputList.end() )
+  {
+  	jt = aReferenceList.begin();
+  	dMinDist = inputSPtr->getExtent(0)*inputSPtr->getExtent(1)*inputSPtr->getExtent(2);
+		while( jt != aReferenceList.end() )
+		{
+			double dDistance = sqrt( ((*it)[0]-(*jt)[0])*((*it)[0]-(*jt)[0])
+				+	((*it)[1]-(*jt)[1])*((*it)[1]-(*jt)[1])
+				+ ((*it)[2]-(*jt)[2])*((*it)[2]-(*jt)[2]) );
+			if ( dMinDist > dDistance )
+				dMinDist = dDistance;
+			++jt;
+		}
+		dSumDist += dMinDist;
+		dMaxMin = std::max( dMaxMin, dMinDist );
+		++it;
+  }
+  double dSumDistA = dSumDist;
+  double dHDA = dMaxMin;
+  dSumDist = 0.0;
+  dMaxMin = 0.0;
+  jt = aReferenceList.begin();
+  cerr << dSumDistA << dMaxMin << "... ";
+  while( jt != aReferenceList.end() )
+  {
+  	it = anInputList.begin();
+  	dMinDist = inputSPtr->getExtent(0)*inputSPtr->getExtent(1)*inputSPtr->getExtent(2);
+		while( it != anInputList.end() )
+		{
+			double dDistance = sqrt( static_cast<double>( ((*it)[0]-(*jt)[0])*((*it)[0]-(*jt)[0])
+				+	((*it)[1]-(*jt)[1])*((*it)[1]-(*jt)[1])
+				+ ((*it)[2]-(*jt)[2])*((*it)[2]-(*jt)[2]) ) );
+			if ( dMinDist > dDistance )
+				dMinDist = dDistance;
+			++it;
+		}
+		dSumDist += dMinDist;
+		dMaxMin = std::max( dMaxMin, dMinDist );
+		++jt;
+  }
+  cerr << dSumDist << dMaxMin << "done." << endl;
+  double dSumDistB = dSumDist;
+  double dHDB = dMaxMin;
+  double dHausdorffDistance = std::max( dHDA, dHDB );
+  double dMeanDistance = ( dSumDistA + dSumDistB ) / ( ulReferenceRegionSize + ulInputRegionSize );
+	parameters.setDouble( "DiceCoefficient", dDiceCoefficient );
+	parameters.setDouble( "TanimotoCoefficient", dTanimotoCoefficient );
+	parameters.setDouble( "HausdorffDistance", dHausdorffDistance );
+	parameters.setDouble( "MeanDistance", dMeanDistance );
+	parameters.setUnsignedLong( "InputRegionSize", ulInputRegionSize );
+	parameters.setUnsignedLong( "ReferenceRegionSize", ulReferenceRegionSize );
+	parameters.setUnsignedLong( "SharedRegionSize", ulSharedRegionSize );
+	parameters.setUnsignedLong( "InputSurface", ulInputSurface );
+	parameters.setUnsignedLong( "ReferenceSurface", ulReferenceSurface );
+	parameters.setUnsignedLong( "CombinedArea", ulCombinedArea );
+	parameters.setUnsignedLong( "FalsePositives", ulFalsePositives );
+	parameters.setUnsignedLong( "FalseNegatives", ulFalseNegatives );
+  return true;
+}
+
+DEFINE_CALL_MACRO( CDiscrepancyMeasures::call, CDiscrepancyMeasures::compute, imageTL )
+
---
+++ trunk/evaltool/src/cdiscrepancymeasures.h
@@ -0,0 +1,60 @@
+//
+// C++ Interface: cdiscrepancymeasures
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef CDISCREPANCYMEASURES_H
+#define CDISCREPANCYMEASURES_H
+#define CDISCREPANCYMEASURES_VERSION "0.1"
+
+// AIPS includes
+#include <cfilter.h>
+
+using namespace aips;
+/** 
+ * Computes different discrepancy measurements for two segmented images.
+ * Inputs (both images need to be of the same type and extent):
+ * 1. Segmentation mask
+ * 2. Reference mask
+ * Outputs:
+ *    See parameters
+ * Parameters:
+ * 1. SegmentationVolume: Volume of segmentation (in voxels)
+ * 2. ReferenceVolume: Volume of reference (in voxels)
+ * 3. DistanceHD: Hausdorff distance
+ * 4. DistanceMS: Mean surface distance
+ * 5. CoeffDice: Dice Coefficient
+ * 6. NoFP: Number of false positives (not normalized)
+ * 7. NoFN: Number of false negatives (not normalized)
+ */
+class CDiscrepancyMeasures : public CFilter
+{
+public:
+/** \name Structors */
+  //@{
+  /// Constructor
+  CDiscrepancyMeasures( ulong ulID )
+    throw();
+  /// Destructor
+  virtual ~CDiscrepancyMeasures()
+    throw();
+  //@}
+/** \name Other methods */
+  //@{
+  /// Reimplemented from CPipelineItem  
+  virtual void apply() 
+    throw();
+  NEW_INSTANCE( CDiscrepancyMeasures )
+  //@}
+private:
+  DECLARE_CALL_MACRO( call )
+  template<typename ImageType> bool compute()
+    throw();
+};
+#endif
